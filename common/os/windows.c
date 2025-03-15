14728145b1d6b237fa49ae189ae76b62a15a89cc
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-03-15 23:03:29 +0100

    Add searching for private key on Windows

--- common/os/windows.c
+++ common/os/windows.c
@@ -1,9 +1,10 @@
 #include "windows.h"
+#include "../util.h"
 
 #include <Windows.h>
 
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * @brief Checks whether the specified file has a correct extension (.pem by default)
@@ -11,14 +12,17 @@
  * @return Boolean value indicating whether the file contains a key
  */
 static uint8_t is_key(char *filename) {
-  char* dot = strrchr(filename, '.');
+  char *dot = strrchr(filename, '.');
 
-  if (!dot) return 0;
+  if (!dot)
+    return 0;
 
   printf("filename: %s\n", filename);
 
-  if (strncmp(dot, KEY_FILE_EXT, 4) == 0) return 1;
-  else return 0;
+  if (strncmp(dot, KEY_FILE_EXT, 4) == 0)
+    return 1;
+  else
+    return 0;
 }
 
 uint8_t windows_search_for_key(const char *path, char *out_file) {
@@ -30,7 +34,8 @@ uint8_t windows_search_for_key(const char *path, char *out_file) {
 
   HANDLE file_handle = FindFirstFileA(find_path, &find_data);
 
-  if (!file_handle) return 0;
+  if (!file_handle)
+    return 0;
 
   do {
     if (is_key(find_data.cFileName)) {
@@ -44,5 +49,25 @@ uint8_t windows_search_for_key(const char *path, char *out_file) {
 
   FindClose(file_handle);
 
+  return 0;
+}
+
+uint8_t find_private_key(const char *out_file) {
+  DWORD drives = GetLogicalDrives();
+  if (drives == 0) {
+    printf("Failed to get drive list.\n");
+    return;
+  }
+
+  for (char letter = 'A'; letter <= 'Z'; letter++) {
+    if (drives & (1 << (letter - 'A'))) {
+      char path[4] = {letter, ':', '\\', '\0'};
+      UINT type = GetDriveTypeA(path);
+      if (type == DRIVE_REMOVABLE) {
+        if (search_for_key(path, out_file) == 1)
+          return 1;
+      }
+    }
+  }
   return 0;
 }
