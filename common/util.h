61699fa82cddd60b6e448bf3270b27ac235457c5
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-03-16 22:53:09 +0100

    Make key search multiplatform (#10)
    * Fix compiling when system specific file is missing
    
    * Make key searching simpler and multiplatform
    
    * Remove unused windows specific key search

--- common/util.h
+++ common/util.h
@@ -3,12 +3,14 @@
 
 #include <stdint.h>
 
+#define KEY_FILE_EXT ".pem"
+
 /**
  * @brief Searches for a key file (.pem) in specified directory
  * @param path Path to the directory
  * @param out_file Buffer where the path to the found file will be stored
  * @return Boolean value indicating whether the file was found or not
  */
-uint8_t search_for_key(const char* path, char* out_file);
+uint8_t search_for_key(const char *path, char *out_file);
 
 #endif
