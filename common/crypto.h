4d5512dd0ed3f4d0bb3e9ca603d827518f44e34a
Kamil Wenta <36380628+kwerenta@users.noreply.github.com>
2025-03-16 08:29:11 +0100

    Add documentation

--- common/crypto.h
+++ common/crypto.h
@@ -65,7 +65,26 @@ void generate_encrypted_RSA_keypair(const char *pin, const char *private_key_fil
  */
 EVP_PKEY *decrypt_and_load_private_key(const char *private_key_file, const char *pin);
 
+/**
+ * @brief Computes hash of provided PDF file content
+ * @param pdf_file Path to PDF file the hash will be computed of
+ * @param hash Buffer where computed hash will be stored
+ * @param hash_len Place where length of computed hash will be stored
+ * @return Boolean value indicating wheter hash has been computed successfully
+ *
+ * @TODO Check wheter hash_len is always the same
+ */
 uint8_t compute_pdf_hash(const char *pdf_file, uint8_t *hash, uint32_t *hash_len);
+/**
+ * @brief Creates signature of hash with provided private key
+ * @param hash Computed hash that will be signed
+ * @param hash_len Length of provided hash
+ * @param private_key Private key that will be used to sign hash
+ * @param sign Buffer where signature will be stored
+ * @param sign_len Place where length of signature will be stored
+ *
+ * @TODO Check wheter sign_len is always the same
+ */
 uint8_t sign_hash(const uint8_t *hash, uint32_t hash_len, EVP_PKEY *private_key, uint8_t *sign, size_t *sign_len);
 
 #endif
