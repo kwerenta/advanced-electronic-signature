8bacf580df31ea66b84b14f034b08f7aa4c8a4c9
mikkelangelas <msugalski20@gmail.com>
2025-03-17 14:39:53 +0100

    Add real-time pendrive detection views

--- common/os/windows.c
+++ common/os/windows.c
@@ -7,12 +7,12 @@ uint8_t find_private_key(char *out_file) {
   DWORD drives = GetLogicalDrives();
   if (drives == 0) {
     printf("Failed to get drive list.\n");
-    return;
+    return 0;
   }
 
   for (char letter = 'A'; letter <= 'Z'; letter++) {
     if (drives & (1 << (letter - 'A'))) {
-      char path[4] = {letter, ':', '\\', '\0'};
+      char path[3] = {letter, ':', '\0'};
       UINT type = GetDriveTypeA(path);
       if (type == DRIVE_REMOVABLE) {
         if (search_for_key(path, out_file) == 1)
