cd6f6f8ea4c061f22eaf1cc644456d82c3950026
Kamil Wenta <kamil.wenta222@gmail.com>
2025-04-03 11:23:01 +0200

    Fix pointer params directions

--- common/crypto.h
+++ common/crypto.h
@@ -25,9 +25,9 @@
  * AES-256 key has fixed size of 256 bits. This function allows to use variable length PINs that will work with AES
  * encryption.
  *
- * @param pin [in] PIN in form of plaintext based on which key and IV will be created
- * @param key [out] Buffer where generated key will be put. **Must** be size of #AES_256_KEY_SIZE
- * @param iv [out] Buffer where generated initialization vector will be put. **Must** be size of #AES_BLOCK_SIZE
+ * @param[in] pin PIN in form of plaintext based on which key and IV will be created
+ * @param[out] key Buffer where generated key will be put. **Must** be size of #AES_256_KEY_SIZE
+ * @param[out] iv Buffer where generated initialization vector will be put. **Must** be size of #AES_BLOCK_SIZE
  * @retval 0 On success
  * @retval PSA_ERROR_CODE On any Mbed TLS function error
  */
@@ -35,12 +35,12 @@ int derive_key_iv(const char *pin, uint8_t *key, uint8_t *iv);
 /**
  * @brief Internal function that simplifies AES encryption process
  * @param decrypt Boolean that determines wheter it should perform decryption or encryption
- * @param key [in] Key that will be used to decrypt/encrypt input data
- * @param iv [in] Initialization Vector that will be used to decrypt/encrypt input data
- * @param input [in] Data to decrypt/encrypt
+ * @param[in] key Key that will be used to decrypt/encrypt input data
+ * @param[in] iv Initialization Vector that will be used to decrypt/encrypt input data
+ * @param[in] input Data to decrypt/encrypt
  * @param input_len Size of input
- * @param output [out] Buffer where decrypted/encrypted data will be stored
- * @param output_len [inout] Length of decrypted/encrypted data. It **must be** initialized with size of output buffer
+ * @param[out] output Buffer where decrypted/encrypted data will be stored
+ * @param[in,out] output_len Length of decrypted/encrypted data. It **must be** initialized with size of output buffer
  * @retval 0 On success
  * @retval PSA_ERROR_CODE On any Mbed TLS function error
  */
@@ -48,11 +48,11 @@ int perform_aes_cipher_operation(uint8_t decrypt, const uint8_t *key, const uint
                                  const size_t input_len, uint8_t *output, size_t *output_len);
 /**
  * @brief Encrypts RSA private key using AES-256-CBC and provided PIN as key
- * @param pin [in] PIN that will be used to encrypt private key
- * @param iv [in] Initialization Vector used in AES-256 encryption
- * @param key [in] Private key in form of plain text that will be encrypted
- * @param ciphertext [out] Buffer where encrypted private key will be stored
- * @param ciphertext_len [inout] Length of generated ciphertext. It **must be** initialized with size of ciphertext
+ * @param[in] pin PIN that will be used to encrypt private key
+ * @param[in] iv Initialization Vector used in AES-256 encryption
+ * @param[in] key Private key in form of plain text that will be encrypted
+ * @param[out] ciphertext Buffer where encrypted private key will be stored
+ * @param[in,out] ciphertext_len Length of generated ciphertext. It **must be** initialized with size of ciphertext
  * buffer
  * @retval 0 On success
  * @retval PSA_ERROR_CODE On any Mbed TLS function error
@@ -61,12 +61,12 @@ int encrypt_private_key(const uint8_t *pin, const uint8_t *iv, const uint8_t *ke
                         size_t *ciphertext_len);
 /**
  * @brief Decrypts RSA private key using provided PIN
- * @param pin [in] PIN that will be used to decrypt private key
- * @param iv [in] Initialization Vector used in AES-256 decryption
- * @param key [in] Private key in form of cipher text that will be decrypted
+ * @param[in] pin PIN that will be used to decrypt private key
+ * @param[in] iv Initialization Vector used in AES-256 decryption
+ * @param[in] key Private key in form of cipher text that will be decrypted
  * @param key_len Size of key
- * @param plaintext [out] Buffer where decrypted private key will be stored
- * @param plaintext_len [inout] Length of generated plaintext. It **must be** initialized with size of plaintext buffer
+ * @param[out] plaintext Buffer where decrypted private key will be stored
+ * @param[in,out] plaintext_len Length of generated plaintext. It **must be** initialized with size of plaintext buffer
  * @retval 0 On success
  * @retval PSA_ERROR_CODE On any Mbed TLS function error
  */
@@ -74,15 +74,15 @@ int decrypt_private_key(const uint8_t *pin, const uint8_t *iv, const uint8_t *ke
                         size_t *plaintext_len);
 /**
  * @brief Generates and saves RSA key pair where private key is encrypted
- * @param pin [in] PIN in plaintext form used to encrypt private key
- * @param private_key_file [in] Path to file where encrypted private key will be stored
- * @param public_key_file [in] Path to file where public key will be stored
+ * @param[in] pin PIN in plaintext form used to encrypt private key
+ * @param[in] private_key_file Path to file where encrypted private key will be stored
+ * @param[in] public_key_file Path to file where public key will be stored
  */
 void generate_encrypted_RSA_keypair(const char *pin, const char *private_key_file, const char *public_key_file);
 /**
  * @brief Loads encrypted private RSA key from PEM file and decrypts it
- * @param pin [in] PIN in plaintext form used to decrypt private key
- * @param private_key_file [in] Path to encrypted private key file
+ * @param[in] pin PIN in plaintext form used to decrypt private key
+ * @param[in] private_key_file Path to encrypted private key file
  * @return Null terminated buffer with decrypted private key or NULL if error occur
  */
 uint8_t *load_encrypted_private_key(const char *pin, const char *private_key_file);
@@ -90,7 +90,7 @@ uint8_t *load_encrypted_private_key(const char *pin, const char *private_key_fil
 /**
  * @brief Computes hash of provided PDF file content
  * @param pdf_file PDF file whose hash will be computed of
- * @param[out] hash Buffer where computed hash will be stored. Need to be size of PSA_HASH_MAX_SIZE (64).
+ * @paramhash Buffer where computed hash will be stored. Need to be size of PSA_HASH_MAX_SIZE (64).
  * @param size Number of bytes in pdf_file starting from beginning that will be used to compute hash. This function will
  * use whole pdf_file size if 0 is provided.
  */
